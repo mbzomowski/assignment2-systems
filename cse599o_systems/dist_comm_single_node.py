7e946c9e2b3dd267ad092b39404602282b93dfcd
Mark Bzomowski <mbzomowski@gmail.com>
2025-11-07 13:14:27 -0800

    Fix

--- cse599o_systems/dist_comm_single_node.py
+++ cse599o_systems/dist_comm_single_node.py
@@ -28,6 +28,8 @@ def distributed_run(rank, world_size, args):
     for r in range(WARMUP_ROUNDS):
         print(f"**********  WARMUP  **********  ROUND {r}")
         dist.all_reduce(data)
+
+    dist.barrier()
     torch.cuda.synchronize()
     t0 = time.time()
     dist.all_reduce(data)
