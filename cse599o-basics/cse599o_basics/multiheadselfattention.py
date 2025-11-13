2542bebeb6b6515e47ed5165e29c8af3ac47a552
Mark Bzomowski <mbzomowski@gmail.com>
2025-11-13 13:11:18 -0800

    Fix multiheadselfattention

--- cse599o-basics/cse599o_basics/multiheadselfattention.py
+++ cse599o-basics/cse599o_basics/multiheadselfattention.py
@@ -20,10 +20,10 @@ class MultiheadSelfAttention(nn.Module):
         self.v_proj_weight = Float[Tensor, " d_k d_in"]
         self.o_proj_weight = Float[Tensor, " d_model d_k"]
 
-        self.q_proj_weight = nn.init.trunc_normal_(self.q_project_weight)
-        self.k_proj_weight = nn.init.trunc_normal_(self.k_project_weight)
-        self.v_proj_weight = nn.init.trunc_normal_(self.v_project_weight)
-        self.o_proj_weight = nn.init.trunc_normal_(self.o_project_weight)
+        self.q_proj_weight = nn.init.trunc_normal_(self.q_proj_weight)
+        self.k_proj_weight = nn.init.trunc_normal_(self.k_proj_weight)
+        self.v_proj_weight = nn.init.trunc_normal_(self.v_proj_weight)
+        self.o_proj_weight = nn.init.trunc_normal_(self.o_proj_weight)
 
         self.max_seq_len = max_seq_len
         self.theta = theta
